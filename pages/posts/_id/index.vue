82b60836ee21cd77d485f521a421bf98e263b4ec
codewithali <ali-raza@ashlarglobal.com>
2021-05-24 12:50:21 +0500

    adding async data to a single post

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,18 +1,39 @@
 <template>
     <div class="single-post-page">
         <section class="post">
-            <h1 class="post-title">Title of the post</h1>
+            <h1 class="post-title">{{loadedPost.title}}</h1>
             <div class="post-details">
-                <div>Last updated on XXX</div>
-                <div>Written by NAME</div>
+                <div>Last updated on {{loadedPost.updatedDate}}</div>
+                <div>Written by {{loadedPost.author}}</div>
             </div>
-            <p class="post-content">Content of the post</p>
+            <p class="post-content">{{loadedPost.content}}</p>
         </section>
         <section class="post-feedback">
             <p>Let me know what you think about the post, send a mail to <a href="mailto:ali@test.com">ali@test.com</a></p>
         </section>
     </div>
 </template>
+
+<script>
+export default {
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPost: {
+          id: '3',
+          title: 'First Post (ID: ' + context.route.params.id + ')',
+          previewText: 'Super amazing, thanks for that!',
+          author: 'Ali Raza',
+          updatedDate: new Date,
+          content: 'Some dumy text which is definitely not the preview text though!',
+          thumbnail: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        }
+      })
+    },1000);
+  }
+}
+</script>
+
 <style scoped>
 .single-post-page {
   padding: 30px;
