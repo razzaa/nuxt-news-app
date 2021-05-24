b0213e60f16ce0e6f6c5ab213af3531d8652dcea
codewithali <ali-raza@ashlarglobal.com>
2021-05-24 13:42:48 +0500

    using promises in asyncdata

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="post-list">
-        <PostList />
+        <PostList :posts="loadedPosts"/>
     </div>
 </template>
 
@@ -9,7 +9,43 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList
-  }
+  },  
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: '1',
+              title: 'First Post',
+              previewText: 'Super amazing, thanks for that!',
+              thumbnail: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            },
+            {
+              id: '2',
+              title: 'Second Post',
+              previewText: 'Super amazing, thanks for that!',
+              thumbnail: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            },
+            {
+              id: '3',
+              title: 'Third Post',
+              previewText: 'Super amazing, thanks for that!',
+              thumbnail: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            },
+          ]
+        })
+      },1000);
+    })
+    .then(data => {
+      return data
+    })
+    .catch(e => {
+      context.error(new Error())
+    })
+    
+  },
+
 }
 </script>
 
