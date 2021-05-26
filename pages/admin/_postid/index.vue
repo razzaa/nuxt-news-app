9a379a4465ac27cfb2501eb31badcfd70845949c
codewithali <ali-raza@ashlarglobal.com>
2021-05-26 11:27:04 +0500

    invalidating token

--- pages/admin/_postid/index.vue
+++ pages/admin/_postid/index.vue
@@ -11,7 +11,7 @@ import AdminPostForm from "@/components/Admin/AdminPostForm";
 
 export default {
   layout: "admin",
-  middleware: 'auth',
+  middleware: ['check-auth', 'auth'],
   components: {
     AdminPostForm
   },
--- store/index.js
+++ store/index.js
@@ -21,6 +21,9 @@ const createStore = () => {
       },
       setToken(state, token) {
         state.token = token
+      },
+      clearToken(state) {
+        state.token = null;
       }
     },
     actions: {
@@ -74,8 +77,26 @@ const createStore = () => {
         }
         ).then(result => {
           vuexContext.commit("setToken", result.idToken);
+          localStorage.setItem("token", result.idToken);
+          localStorage.setItem("tokenExpiration", new Date().getTime() + result.expiresIn * 1000);
+          vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000)
         })
         .catch(e => console.log(e));
+      },
+      setLogoutTimer(vuexContext, duration) {
+        setTimeout(() => {
+          vuexContext.commit('clearToken')
+        }, duration);
+      },
+      initAuth(vuexContext) {
+        const token = localStorage.getItem("token");
+        const expirationDate = localStorage.getItem("tokenExpiration");
+
+        if (new Date().getTime() > +expirationDate || !token) {
+          return;
+        }
+        vuexContext.dispatch("setLogoutTimer", +expirationDate - new Date().getTime());
+        vuexContext.commit("setToken", token);
       }
     },
     getters: {
